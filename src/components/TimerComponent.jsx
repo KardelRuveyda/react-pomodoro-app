a93729b584c628dd8226f16b0ebb14ae92b09a01
Kardel Ruveyda <33912144+KardelRuveyda@users.noreply.github.com>
2024-03-10 23:42:00 +0300

    Update TimerComponent.jsx

--- src/components/TimerComponent.jsx
+++ src/components/TimerComponent.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 
-const TimerComponent = () => {
+function TimerComponent () {
   const [time, setTime] = useState(25 * 60); // Saniye cinsinden süre
   const [isActive, setIsActive] = useState(false);
 
@@ -13,7 +13,7 @@ const TimerComponent = () => {
         if (time <= 0) {
           clearInterval(intervalId);
           setIsActive(false);
-          // playDefaultNotificationSound(); 
+          playDefaultNotificationSound(); 
         } else {
           setTime((prevTime) => prevTime - 1);
         }
@@ -29,7 +29,20 @@ const TimerComponent = () => {
 
   const resetTimer = () => {
     setIsActive(false);
-    setTime(25 * 60); // Saniye cinsinden 25 dakikaa
+    setTime(25 * 60); // Saniye cinsinden 25 dakika
+  };
+
+  const playDefaultNotificationSound = () => {
+    try {
+      const audioContext = new (window.AudioContext ||
+        window.webkitAudioContext)();
+      const oscillator = audioContext.createOscillator();
+      oscillator.connect(audioContext.destination);
+      oscillator.start();
+      oscillator.stop(audioContext.currentTime + 0.5);
+    } catch (error) {
+      console.error('Varsayılan ses çalınırken bir hata oluştu:', error);
+    }
   };
 
   const formattedTime = moment.utc(time * 1000).format('mm:ss');
